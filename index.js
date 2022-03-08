8db16d897a322fbf49b65435d24811a8f5b8d6f5
JiiXaa <anythingfx@gmail.com>
2022-03-08 12:47:35 +0000

    added port process.env for heroku deployment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 5000;
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -19,7 +21,7 @@ app.use(
 );
 app.use(cookieParser());
 
-app.listen(5000, () => console.log('server started on port 5000'));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
 // set up routers
 
